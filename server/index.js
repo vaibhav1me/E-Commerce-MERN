86c87090fef3e507d6cd3c66fa9758151f086195
vaibhav1me <vaibhav1@me.iitr.ac.in>
2023-12-28 19:48:37 +0530

    modified version in vercel.json

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
 require("dotenv").config();
 const connectDb = require('./config/dbConnect')
 const userRoutes = require('./routes/Users')
@@ -11,21 +10,29 @@ const orderRoutes = require('./routes/Orders')
 const categoryRoutes = require('./routes/Category');
 const { authenticateUser, checkCurrentUser } = require("./middlewares/AuthMiddleware");
 const PORT = process.env.PORT || 3000
+/* -----ToDo-----
+const cookieParser = require('cookie-parser')
+ */
+
 // const  = require("./middlewares/AuthMiddleware");
 
 app.use(cors({credentials: true}))
 app.use(express.json())
-app.use(cookieParser())
+
+/* Todo ---------
+app.use(cookieParser())  */
 
 app.use('/api/users', userRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/carts', authenticateUser, cartRoutes)
 app.use('/api/orders', authenticateUser, orderRoutes)
-// app.use('/api/carts', cartRoutes)
-// app.use('/api/orders', orderRoutes)
 app.use('/api/category', categoryRoutes)
 app.post('/api', checkCurrentUser)
 
+
+app.get('/', (req, res) => {
+    res.json({message: "Congratulations, the server is running successfully"})
+})
 const startApp = async () => {
     try {
         await connectDb(process.env.MONGO_URI);
