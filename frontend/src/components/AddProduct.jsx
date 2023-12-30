19f11a8abb135c6c465b8f00859571bc9455123a
vaibhav1me <vaibhav1@me.iitr.ac.in>
2023-12-30 23:16:23 +0530

    styled edit product and add product

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -3,142 +3,246 @@ import { LoginContext } from "../context/DataProvider";
 import { createProduct, fetchAllCategories } from "../apis/api";
 import { useNavigate } from "react-router";
 
-
 const AddProduct = () => {
-    const {user, setUser} = useContext(LoginContext)
-    const [categories, setCategories] = useState([])
-    const [message, setMessage] = useState("")
-    const [brands, setBrands] = useState([])
-    const [product, setProduct] = useState({title: "", price: "", stock: 0, seller: user.name, description: "", category: "", brand: "", images: []})
-    const [images, setImages] = useState(["", "", "", ""])
-    const navigate = useNavigate()
-    
-    useEffect(() => {
-        // console.log(user)
-        setProduct({...product, seller: user.name})
-    }, [user])
+  const { user, setUser } = useContext(LoginContext);
+  const [categories, setCategories] = useState([]);
+  const [message, setMessage] = useState("");
+  const [brands, setBrands] = useState([]);
+  const [product, setProduct] = useState({
+    title: "",
+    price: "",
+    stock: 0,
+    seller: user.name,
+    description: "",
+    category: "",
+    brand: "",
+    images: [],
+  });
+  const [images, setImages] = useState(["", "", "", ""]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchCategories = async () => {
-           const response = await fetchAllCategories()
-        //    console.log(response.data)
-           setCategories(response.data)
-        }
-        fetchCategories()
-    }, [])
+  useEffect(() => {
+    setProduct({ ...product, seller: user.name });
+  }, [user]);
 
-    const updateProduct = (e) => {
-        setProduct({...product, [e.target.name]: e.target.value})
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await fetchAllCategories();
+      setCategories(response.data);
+    };
+    fetchCategories();
+  }, []);
+
+  const updateProduct = (e) => {
+    setProduct({ ...product, [e.target.name]: e.target.value });
+  };
+
+  const updateImages = (index, value) => {
+    const updatedImages = [...images];
+    updatedImages[index] = value;
+    setImages(updatedImages);
+  };
+
+  const addProduct = async () => {
+    const response = await createProduct(product);
+    if (response.data.message) {
+      setMessage(response.data.message);
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
+    } else {
+      navigate("/account/products");
     }
+  };
 
-    const updateImages = (index, value) => {
-        const updatedImages = [...images]
-        updatedImages[index] = value;
-        setImages(updatedImages)
+  useEffect(() => {
+    setProduct({ ...product, images: images });
+  }, [images]);
+
+  useEffect(() => {
+    const category = categories.filter(
+      (category) => category.categoryName === product.category
+    );
+    setProduct({ ...product, brand: "" });
+    if (category[0]) {
+      setBrands(category[0].brands);
     }
-
-    const addProduct = async () => {
-        const response = await createProduct(product)
-        if (response.data.message) {
-            setMessage(response.data.message)
-            setTimeout(() => {
-                setMessage("")
-            }, 3000);
-        }
-        else {
-            navigate('/account/products')
-        }
-        // console.log(response.data)
-    }
-
-    useEffect(() => {
-        setProduct({...product, images: images})
-    }, [images])
-
-    useEffect(() => {
-        // console.log(product)
-    },[product])
-
-    useEffect(() => {
-        const category = categories.filter((category) => category.categoryName === product.category)
-        // setBrands([])
-        setProduct({...product, brand: ""})
-        if (category[0]) {
-        //   console.log(category[0].brands);
-          setBrands(category[0].brands)
-        }
-    }, [product.category])
-
-    // useEffect(() => {
-    //     const fetch
-    // }, [product.category])
-    // useEffect(() =>{
-    //     console.log(user)
-    // }, [user])
+  }, [product.category]);
 
   return (
-    <div className="text-black">
-      <label htmlFor="category">Category</label>
-      <select name="category" id="category" value={product.category} onChange={(e) => {updateProduct(e)}}>
-        <option value={''} className="text-black">Select a Category</option>
-        {
-            categories.map((category) => {
-                return (<option value={category.categoryName} className="text-black">{category.categoryName}</option>)
-            })
-        }
-      </select>
+    <section className="bg-secondary flex flex-col rounded-md text-center py-4">
+      <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+        <label htmlFor="category" className="text-yellow medium:mx-3 mr-1">
+          Category:{" "}
+        </label>
+        <select
+          name="category"
+          id="category"
+          value={product.category}
+          onChange={(e) => {
+            updateProduct(e);
+          }}
+          className="bg-primary p-2 rounded-md "
+        >
+          <option value={""} >
+            Select a Category
+          </option>
+          {categories.map((category) => {
+            return (
+              <option value={category.categoryName} className="text-yellow">
+                {category.categoryName}
+              </option>
+            );
+          })}
+        </select>
+      </div>
 
-      <label htmlFor="brand">Brand</label>
-      <select name="brand" id="brand" onChange={(e) => {updateProduct(e)}} value={product.brand}>
-        {
-            product.category === "" ? <option>Select a category first</option> : <>
-            <option value={''} className="text-black">Select a brand</option>
-            {Array.isArray(brands) && brands.map((brand) => {
-                return (<option value={brand} className="text-black">{brand}</option>)
-            })}</>
-        }
-      </select>
+      <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+        <label htmlFor="brand" className="text-yellow medium:mx-3 mr-1">
+          Brand:{" "}
+        </label>
+        <select
+          name="brand"
+          id="brand"
+          onChange={(e) => {
+            updateProduct(e);
+          }}
+          value={product.brand}
+          className="bg-primary p-2 rounded-md "
+        >
+          {product.category === "" ? (
+            <option>Select a category first</option>
+          ) : (
+            <>
+              <option value={""} className="">
+                Select a brand
+              </option>
+              {Array.isArray(brands) &&
+                brands.map((brand) => {
+                  return (
+                    <option value={brand} className="text-yellow">
+                      {brand}
+                    </option>
+                  );
+                })}
+            </>
+          )}
+        </select>
+      </div>
 
-      <label htmlFor="description">Description</label>
-      <input type="text" name="description" id="description" value={product.description} onChange={(e) => {updateProduct(e)}}/>
+      <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+        <label htmlFor="description" className="text-yellow medium:mx-3 mr-1">
+          Description:{" "}
+        </label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          value={product.description}
+          onChange={(e) => {
+            updateProduct(e);
+          }}
+          className="bg-primary p-2 rounded-md "
+        />
+      </div>
 
-      <label htmlFor="price">Price</label>
-      <input type="text" name="price" id="price" value={product.price} onChange={(e) => {updateProduct(e)}}/>
+      <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+        <label htmlFor="price" className="text-yellow medium:mx-3 mr-1">
+          Price:{" "}
+        </label>
+        <input
+          type="text"
+          name="price"
+          id="price"
+          value={product.price}
+          onChange={(e) => {
+            updateProduct(e);
+          }}
+          className="bg-primary p-2 rounded-md "
+        />
+      </div>
 
-      <label htmlFor="seller">Seller</label>
-      <input type="text" name="seller" id="seller"  value={user.name} readOnly/>
+      <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+        <label htmlFor="seller" className="text-yellow medium:mx-3 mr-1">
+          Seller:{" "}
+        </label>
+        <input
+          type="text"
+          name="seller"
+          id="seller"
+          value={user.name}
+          readOnly
+          className="bg-primary p-2 rounded-md "
+        />
+      </div>
 
-      <label htmlFor="title">Title</label>
-      <input type="text" name="title" id="title" value={product.title} onChange={(e) => {updateProduct(e)}}/>
+      <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+        <label htmlFor="title" className="text-yellow medium:mx-3 mr-1">
+          Title:{" "}
+        </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={product.title}
+          onChange={(e) => {
+            updateProduct(e);
+          }}
+          className="bg-primary p-2 rounded-md "
+        />
+      </div>
 
-      <label htmlFor="stock">Stock</label>
-      <input type="text" name="stock" id="stock" value={product.stock} onChange={(e) => {updateProduct(e)}} placeholder="0"/>
+      <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+        <label htmlFor="stock" className="text-yellow medium:mx-3 mr-1">
+          Stock:{" "}
+        </label>
+        <input
+          type="text"
+          name="stock"
+          id="stock"
+          value={product.stock}
+          onChange={(e) => {
+            updateProduct(e);
+          }}
+          placeholder="0"
+          className="bg-primary p-2 rounded-md "
+        />
+      </div>
 
       {/* brand, category, description, images, price, seller, stock, title, */}
-      {
-        images.map((image, index) => {
-            return (
-                <>
-                    <label htmlFor={`image${index+1}`}>ImageUrl - {index+1}</label>
-                    <input type="text" id={`image${index+1}`} onChange={(e) => {updateImages(index, e.target.value)}}/>
-                </>
-            )
-        })
-      }
-      {/* <label htmlFor="image1">ImageUrl - 1</label>
-    <input type="text" name="images" id="image1" value={product.images[0]}/>
+      {images.map((image, index) => {
+        return (
+          <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+            <label
+              htmlFor={`image${index + 1}`}
+              className="text-yellow medium:mx-3 mr-1"
+            >
+              ImageUrl - {index + 1}:{" "}
+            </label>
+            <input
+              type="text"
+              id={`image${index + 1}`}
+              onChange={(e) => {
+                updateImages(index, e.target.value);
+              }}
+              placeholder={`${index != 0 ? 'optional' : ""}`}
+              className="bg-primary p-2 rounded-md "
+            />
+          </div>
+        );
+      })}
 
-      <label htmlFor="image2">ImageUrl - 2</label>
-    <input type="text" name="images" id="image2" value={product.images[1]}/>
-
-      <label htmlFor="image3">ImageUrl - 3</label>
-    <input type="text" name="images" id="image3" value={product.images[2]}/>
-
-      <label htmlFor="image4">ImageUrl - 4</label>
-    <input type="text" name="images" id="image4" value={product.images[3]}/> */}
-    <div><button onClick={() => {addProduct()}}>Confirm</button></div>
-    <div className="text-black">{message != "" && message}</div>
-    </div>
+      <div>
+        <button
+          onClick={() => {
+            addProduct();
+          }}
+        className="bg-primary text-yellow hover:bg-tertiary hover:text-primary px-2 py-1 rounded-md">
+          Confirm
+        </button>
+      </div>
+      <div className="text-yellow">{message != "" && message}</div>
+    </section>
   );
 };
 
--- frontend/src/components/EditProduct.jsx
+++ frontend/src/components/EditProduct.jsx
@@ -80,51 +80,66 @@ const EditProduct = () => {
     }
 
   return (
-    <div className="text-black">
-      <label htmlFor="category">Category</label>
-      <select name="category" id="category" value={product.category} onChange={(e) => {updateProductDetails(e)}}>
-        <option value={''} className="text-black">Select a Category</option>
+    <section className="bg-secondary flex flex-col rounded-md text-center py-4">
+        
+      <div className='my-3 medium:text-[1.2rem] text-[4vw]'>
+      <label htmlFor="category" className='text-yellow medium:mx-3 mr-1'>Category: </label>
+      <select name="category" id="category" className='bg-primary p-2 rounded-md ' value={product.category} onChange={(e) => {updateProductDetails(e)}}>
+        <option value={''} >Select a Category</option>
         {
-            categories.map((category) => {
-                return (<option value={category.categoryName} className="text-black">{category.categoryName}</option>)
-            })
+          categories.map((category) => {
+            return (<option value={category.categoryName} className="text-yellow">{category.categoryName}</option>)
+          })
         }
       </select>
+        </div>
 
-      <label htmlFor="brand">Brand</label>
-      <select name="brand" id="brand" onChange={(e) => {updateProductDetails(e)}} value={product.brand}>
+<div className="my-3 medium:text-[1.2rem] text-[4vw]">
+      <label htmlFor="brand" className="text-yellow medium:mx-3 mr-1">Brand: </label>
+      <select name="brand" id="brand" className="bg-primary p-2 rounded-md " onChange={(e) => {updateProductDetails(e)}} value={product.brand}>
         {
-            product.category === "" ? <option>Select a category first</option> : <>
+          product.category === "" ? <option>Select a category first</option> : <>
             <option value={''} className="text-black">Select a brand</option>
             {Array.isArray(brands) && brands.map((brand) => {
-                return (<option value={brand} className="text-black" >{brand}</option>)
+              return (<option value={brand} className="text-yellow" >{brand}</option>)
             })}</>
-        }
+          }
       </select>
+          </div>
 
-      <label htmlFor="description">Description</label>
-      <input type="text" name="description" id="description" value={product.description} onChange={(e) => {updateProductDetails(e)}}/>
+        <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+      <label htmlFor="description" className="text-yellow medium:mx-3 mr-1">Description: </label>
+      <input type="text" name="description" id="description" className="bg-primary p-2 rounded-md " value={product.description} onChange={(e) => {updateProductDetails(e)}}/>
+        </div>
 
-      <label htmlFor="price">Price</label>
-      <input type="text" name="price" id="price" value={product.price} onChange={(e) => {updateProductDetails(e)}}/>
+<div className="my-3 medium:text-[1.2rem] text-[4vw]">
+      <label htmlFor="price" className="text-yellow medium:mx-3 mr-1">Price: </label>
+      <input type="text" name="price" id="price" className="bg-primary p-2 rounded-md " value={product.price} onChange={(e) => {updateProductDetails(e)}}/>
+</div>
 
-      <label htmlFor="seller">Seller</label>
-      <input type="text" name="seller" id="seller"  value={user.name} readOnly/>
+<div className="my-3 medium:text-[1.2rem] text-[4vw]">
+      <label htmlFor="seller" className="text-yellow medium:mx-3 mr-1">Seller: </label>
+      <input type="text" name="seller" id="seller" className="bg-primary p-2 rounded-md "  value={user.name} readOnly/>
+</div>
 
-      <label htmlFor="title">Title</label>
-      <input type="text" name="title" id="title" value={product.title} onChange={(e) => {updateProductDetails(e)}}/>
+<div className="my-3 medium:text-[1.2rem] text-[4vw]">
+      <label htmlFor="title" className="text-yellow medium:mx-3 mr-1">Title: </label>
+      <input type="text" name="title" id="title" className="bg-primary p-2 rounded-md " value={product.title} onChange={(e) => {updateProductDetails(e)}}/>
+</div>
 
-      <label htmlFor="stock">Stock</label>
-      <input type="text" name="stock" id="stock" value={product.stock} onChange={(e) => {updateProductDetails(e)}} placeholder="0"/>
+<div className="my-3 medium:text-[1.2rem] text-[4vw]">
+      <label htmlFor="stock" className="text-yellow medium:mx-3 mr-1">Stock: </label>
+      <input type="text" name="stock" id="stock" className="bg-primary p-2 rounded-md " value={product.stock} onChange={(e) => {updateProductDetails(e)}} placeholder="0"/>
+</div>
 
       {/* brand, category, description, images, price, seller, stock, title, */}
       {
         images.map((image, index) => {
             return (
-                <>
-                    <label htmlFor={`image${index+1}`}>ImageUrl - {index+1}</label>
-                    <input type="text" id={`image${index+1}`} onChange={(e) => {updateImages(index, e.target.value)}} value={product.images[index]}/>
-                </>
+                <div className="my-3 medium:text-[1.2rem] text-[4vw]">
+                    <label htmlFor={`image${index+1}`} className="text-yellow medium:mx-3 mr-1">ImageUrl - {index+1}: </label>
+                    <input type="text" id={`image${index+1}`} onChange={(e) => {updateImages(index, e.target.value)}} className="bg-primary p-2 rounded-md " value={product.images[index]}/>
+                </div>
             )
         })
       }
@@ -139,11 +154,11 @@ const EditProduct = () => {
 
       <label htmlFor="image4">ImageUrl - 4</label>
     <input type="text" name="images" id="image4" value={product.images[3]}/> */}
-    <div><button onClick={() => {editProduct()}}>Confirm</button></div>
-    <div><button onClick={() => {removeProduct()}}>Delete Product</button>
+    <div><button className="bg-primary text-yellow hover:bg-tertiary hover:text-primary px-2 py-1 rounded-md mb-2" onClick={() => {editProduct()}}>Confirm</button></div>
+    <div><button className="bg-primary text-yellow hover:bg-tertiary hover:text-primary px-2 py-1 rounded-md mb-2" onClick={() => {removeProduct()}}>Delete Product</button>
     </div>
     <div className="text-black">{message != "" && message}</div>
-    </div>
+    </section>
   )
 }
 
